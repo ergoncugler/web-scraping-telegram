7706e55b37a75bb4b9f1a0bc2c42d271ca9e75c7
Ergon Cugler <ergon@usp.br>
2024-08-07 22:35:41 -0300

    Update scrape_and_filter_by_keywords_from_parquet_to_excel.py

--- scrape_and_filter_by_keywords_from_parquet_to_excel.py
+++ scrape_and_filter_by_keywords_from_parquet_to_excel.py
@@ -4,42 +4,42 @@ import os
 import json
 
 def filter_and_save_by_keywords(folder_path, input_filename, output_filename, content_col, keywords, max_rows_per_file):
-    """
-    Filters the rows based on keywords in the specified column, adds a column for each keyword indicating its presence,
-    and saves the result to new Excel files if the maximum number of rows is exceeded.
+    
+    # Filters the rows based on keywords in the specified column, adds a column for each keyword indicating its presence,
+    # and saves the result to new Excel files if the maximum number of rows is exceeded.
 
-    Parameters:
-    folder_path (str): The path to the folder containing the Parquet file.
-    input_filename (str): The name of the input Parquet filename.
-    content_col (str): The column name containing the content data.
-    keywords (list): The list of keywords to filter the content.
-    output_filename (str): The base name of the output Excel file.
-    max_rows_per_file (int): The maximum number of rows per output file.
+    # Parameters:
+    # folder_path (str): The path to the folder containing the Parquet file.
+    # input_filename (str): The name of the input Parquet filename.
+    # content_col (str): The column name containing the content data.
+    # keywords (list): The list of keywords to filter the content.
+    # output_filename (str): The base name of the output Excel file.
+    # max_rows_per_file (int): The maximum number of rows per output file.
 
-    Returns:
-    None
+    # Returns:
+    # None
 
-    Steps:
-    1. Load the Parquet file into a DataFrame.
-    2. Decode the 'Comments List' column from JSON, if present.
-    3. Create a new column for each keyword indicating its presence in the content.
-    4. Add a column that counts the number of keywords found in each row.
-    5. Filter the DataFrame to include only rows where at least one keyword was found.
-    6. Split and save the filtered DataFrame into multiple Excel files if necessary.
+    # Steps:
+    # 1. Load the Parquet file into a DataFrame.
+    # 2. Decode the 'Comments List' column from JSON, if present.
+    # 3. Create a new column for each keyword indicating its presence in the content.
+    # 4. Add a column that counts the number of keywords found in each row.
+    # 5. Filter the DataFrame to include only rows where at least one keyword was found.
+    # 6. Split and save the filtered DataFrame into multiple Excel files if necessary.
 
-    Usage:
-    Place the Parquet file to be filtered in the specified folder path and specify the appropriate column names, keywords, output file name, and maximum number of rows per file.
+    # Usage:
+    # Place the Parquet file to be filtered in the specified folder path and specify the appropriate column names, keywords, output file name, and maximum number of rows per file.
 
-    Example:
-    filter_and_save_by_keywords(
-        folder_path=r'C:\Users\Public\PyCharmProjects\Data_Conspira',
-        input_filename="unified_data_telegram.parquet",
-        content_col='Content',
-        keywords=['Trump', 'Biden', 'Kamala'],
-        output_filename='filtered_keywords',
-        max_rows_per_file=1000000
-    )
-    """
+    # Example:
+    # filter_and_save_by_keywords(
+    #     folder_path=r'C:\Users\Public\PyCharmProjects\Data_Conspira',
+    #     input_filename="unified_data_telegram.parquet",
+    #     content_col='Content',
+    #     keywords=['Trump', 'Biden', 'Kamala'],
+    #     output_filename='filtered_keywords',
+    #     max_rows_per_file=1000000
+    # )
+    
     try:
         # Combine folder path and input filename to get the full file path
         input_file_path = os.path.join(folder_path, input_filename)
@@ -84,6 +84,7 @@ def filter_and_save_by_keywords(folder_path, input_filename, output_filename, co
             print(f"Filtered file saved at: {output_path}")
     except Exception as e:
         print(f"An error occurred: {e}")
+
 
 # Usage
 folder_path = r'C:\Users\Public\PyCharmProjects\Data_Conspira' # Example
